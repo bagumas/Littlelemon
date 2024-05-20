cdb1553becebcbde44f64216e93e8e2f2d876ac2
Samuel Baguma <SamuelBaguma@MacBook-Pro-Sam.local>
2024-05-20 16:22:58 -0700

    added models

--- Littlelemon/settings.py
+++ Littlelemon/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import pymysql
+pymysql.install_as_MySQLdb()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -76,8 +78,12 @@ WSGI_APPLICATION = 'Littlelemon.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'reservations',
+        'HOST' : '127.0.0.1',
+        'PORT' : '3306',
+        'USER' : 'root',
+        'PASSWORD' : 'Chicago1@',
     }
 }
 
--- restaurant/models.py
+++ restaurant/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Menu(models.Model):
+    title = models.CharField(max_length=255, db_index=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2,  db_index=True)
+    inventory = models.IntegerField()
+
+
+
+class Book(models.Model):
+    name = models.CharField(max_length=255, db_index=True)
+    no_of_guests = models.IntegerField()
+    bookingdate = models.DateTimeField
